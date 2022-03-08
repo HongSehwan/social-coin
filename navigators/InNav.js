a2b7ce2fa9a70c473c482719e9b503fd7b162519
Hong <gg9297@gmail.com>
2022-03-08 18:00:44 +0900

    Fix data & Add Detail.js

--- navigators/InNav.js
+++ navigators/InNav.js
@@ -1,12 +1,23 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Detail from "../screens/Detail";
 import Home from "../screens/Home";
+import { BLACK_COLOR } from "../colors";
 
 const Nav = createNativeStackNavigator();
 
 const InNav = () => (
-  <Nav.Navigator>
-    <Nav.Screen name="Home" component={Home} />
+  <Nav.Navigator
+    screenOptions={{
+      presentation: "modal",
+      headerTintColor: "white",
+      headerStyle: {
+        backgroundColor: BLACK_COLOR,
+      },
+    }}
+  >
+    <Nav.Screen name="Coin" component={Home} />
+    <Nav.Screen name="Detail" component={Detail} />
   </Nav.Navigator>
 );
 
--- screens/Join.js
+++ screens/Join.js
@@ -1,6 +1,8 @@
 import React, { useRef, useState } from "react";
 import styled from "styled-components/native";
+import auth from "@react-native-firebase/auth";
 import { BLACK_COLOR } from "../colors";
+import { ActivityIndicator, Alert } from "react-native";
 
 const Container = styled.View`
   background-color: ${BLACK_COLOR};
@@ -36,8 +38,27 @@ const Join = () => {
   const passwordInput = useRef();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const onSubmitEditing = () => {
+  const [loading, setLoading] = useState(false);
+  const onSubmitEmailEditing = () => {
     passwordInput.current.focus();
+  };
+  const onSubmitPasswordEditing = async () => {
+    if (email === "" || password === "") {
+      return Alert.alert("Fill in the form.");
+    }
+    if (loading) {
+      return;
+    }
+    setLoading(true);
+    try {
+      await auth().createUserWithEmailAndPassword(email, password);
+    } catch (e) {
+      switch (e.code) {
+        case "auth/weak-password": {
+          Alert.alert("Write a stronger password!");
+        }
+      }
+    }
   };
   return (
     <Container>
@@ -50,7 +71,7 @@ const Join = () => {
         returnKeyType="next"
         value={email}
         onChangeText={(text) => setEmail(text)}
-        onSubmitEditing={onSubmitEditing}
+        onSubmitEditing={onSubmitEmailEditing}
       />
       <TextInput
         ref={passwordInput}
@@ -60,9 +81,14 @@ const Join = () => {
         returnKeyType="done"
         value={password}
         onChangeText={(text) => setPassword(text)}
+        onSubmitEditing={onSubmitPasswordEditing}
       />
-      <Btn>
-        <BtnText>Create Account</BtnText>
+      <Btn onPress={onSubmitPasswordEditing}>
+        {loading ? (
+          <ActivityIndicator color="white" />
+        ) : (
+          <BtnText>Create Account</BtnText>
+        )}
       </Btn>
     </Container>
   );
